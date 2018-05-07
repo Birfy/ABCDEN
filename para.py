5d213c32713f32f6e17e3e93c9f45887e0d85608
Birfy <zzcdy@126.com>
2018-05-08 01:12:03 +0800

    add path

--- para.py
+++ para.py
@@ -2,10 +2,19 @@ from tkinter import *
 import os
 
 root = Tk()
+root.resizable(width=False, height=False)
 root.title("Para Generator")
 
-labels = ['Initiation method','Anderson Mixing Option','Max Normal Mixing Steps','wopt','wcmp','maxErr','(ly/lx)^2','(lz/lx)^2','Matrix','hAB','hBC','hAC','fA','fB','fC','lx','ly','lz','Nx','Ny','Nz','Parameters Filename','Concentration Filename','ds0','epA','epB','epC']
-defaults = ['0','1','100','0.050','0.100','0.001','1', '1','0','40.0', '40.0', '40.0','0.14', '0.26', '0.60','4.000', '4.000', '4.000','64', '64', '64','fet.dat','pha.dat','0.005','1', '1', '1']
+labels = ['Initiation method','Anderson Mixing Option','Max Normal Mixing Steps','wopt','wcmp','maxErr','(ly/lx)^2','(lz/lx)^2','Matrix','hAB','hBC','hAC','fA','fB','fC','lx','ly','lz','Nx','Ny','Nz','Parameters Filename','Concentration Filename','ds0','epA','epB','epC','path']
+defaults=[]
+result=[]
+
+para = open('para','r')
+for item in para.readlines():
+    for element in item.strip('\n').split(', '):
+        defaults.append(element)
+para.close()
+
 entry=[]
 frame = Frame(root)
 buttonframe = Frame(root)
@@ -21,8 +30,9 @@ def generate():
     result=[]
     for i in range(len(labels)):
         result.append(entry[i].get().strip())
+
     para = open('para','w')
-    columns=[1,2,2,1,2,1,3,3,3,3,1,1,1,3]
+    columns=[1,2,2,1,2,1,3,3,3,3,1,1,1,3,1]
     index=0
     for item in columns:
         for i in range(item):
@@ -33,7 +43,6 @@ def generate():
                 para.writelines('\n')
             index+=1
     para.close()
-    os.system("echo PARA GENERATED")
     resultText.insert(END,"PARA GENERATED\n")
 
 def update():
@@ -42,9 +51,25 @@ def update():
     
 
 def run():
-    resultText.insert(END,"RUNNING\n")
+    result=[]
+    for i in range(len(labels)):
+        result.append(entry[i].get().strip())
+
+    runfile = open('run.bashrc','r')
+    lines = runfile.readlines()
+
+    runfile = open('run.bashrc','w')
+    for i in lines:
+        runfile.write(i.replace('$path',result[len(result)-1]))
+    runfile.close()
+
     p = os.popen("bash run.bashrc")
     resultText.insert(END,p.read())
+
+    runfile = open('run.bashrc','w')
+    for i in lines:
+        runfile.write(i.replace(result[len(result)-1],'$path'))
+    runfile.close()
 
 Button(buttonframe, text='generate', command = generate).grid(row=0,column=0,padx=5)
 Button(buttonframe, text='update', command = update).grid(row=0,column=1,padx=5)
--- run.bashrc
+++ run.bashrc
@@ -1,2 +1,2 @@
-ssh cdy@10.158.131.62 "cp ./update/abcden ./; cp ./update/para ./; rsh c0103 'nohup ./abcden >/dev/null 2>&1 &'"
-echo 'ABCDEN IS RUNNING'
+ssh cdy@10.158.131.62 "mkdir $path;cp ./update/abcden $path;cp ./update/para $path;rsh c0103 'cd $path;nohup ./abcden >/dev/null 2>&1 &'"
+echo "ABCDEN IS RUNNING"
